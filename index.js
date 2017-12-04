93e038a62b3504bdf931e35a57454c90058c9004
aleclarson <alec.stanford.larson@gmail.com>
2017-12-04 17:19:36 -0500

    fix: export the `liquid` function by default
    This function creates an `Engine` just like `new Engine()`.

--- index.js
+++ index.js
@@ -1,81 +1,13 @@
-const Scope = require('./src/scope')
-const tokenizer = require('./src/tokenizer')
-const Render = require('./src/render')
-const lexical = require('./src/lexical')
-const TagCache = require('./src/tag')
-const FilterCache = require('./src/filter')
-const Parser = require('./src/parser')
-const Syntax = require('./src/syntax')
-const tags = require('./tags')
-const filters = require('./filters')
-const anySeries = require('./src/util/promise').anySeries
-const Errors = require('./src/util/error')
 
-function Engine(options) {
-  options = Object.assign({
-    trim_right: false,
-    trim_left: false,
-    strict_filters: false,
-    strict_variables: false
-  }, options)
+const Liquid = require('./src/engine')
 
-  this.options = options
-  this.tag = new TagCache(this)
-  this.filter = new FilterCache(this, options)
-  this.parser = Parser(tag, filter)
-  this.renderer = Render()
-
-  tags(this)
-  filters(this)
-
-  return this
+function liquid(options) {
+  return new Liquid(options)
 }
 
-Engine.prototype = {
-  constructor: Engine,
-  parse: function (html, filepath) {
-    var tokens = tokenizer.parse(html, filepath, this.options)
-    return this.parser.parse(tokens)
-  },
-  render: function (tpl, ctx, opts) {
-    opts = Object.assign({}, this.options, opts)
-    var scope = Scope.factory(ctx || {}, opts)
-    return this.renderer.renderTemplates(tpl, scope)
-  },
-  parseAndRender: function (html, ctx, opts) {
-    return Promise.resolve()
-      .then(() => this.parse(html))
-      .then(tpl => this.render(tpl, ctx, opts))
-      .catch(e => {
-        if (e instanceof Errors.RenderBreakError) {
-          return e.html
-        }
-        throw e
-      })
-  },
-  evalOutput: function (str, scope) {
-    var tpl = this.parser.parseOutput(str.trim())
-    return this.renderer.evalOutput(tpl, scope)
-  },
-  registerFilter: function (name, filter) {
-    return this.filter.register(name, filter)
-  },
-  registerTag: function (name, tag) {
-    return this.tag.register(name, tag)
-  }
-}
+liquid.Engine = Liquid
 
-Object.assign(Engine, {
-  lexical: lexical,
-  isTruthy: Syntax.isTruthy,
-  isFalsy: Syntax.isFalsy,
-  evalExp: Syntax.evalExp,
-  evalValue: Syntax.evalValue,
-  Types: {
-    ParseError: Errors.ParseError,
-    TokenizationEroor: Errors.TokenizationError,
-    RenderBreakError: Errors.RenderBreakError,
-  }
-})
+// Copy all static properties of `Liquid`
+Object.assign(liquid, Liquid)
 
-module.exports = Engine
+module.exports = liquid
